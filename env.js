3e5f74a13ddd4f64340a4e49f65cd5be1278defe
Scott Ballantyne <scottballantyne@protonmail.com>
2025-09-14 12:41:16 -0500

    maybe this is a good place for this?

---
+++ env.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const fs   = require('fs');
+var dotenv = require('dotenv');
+
+if (process.env.NODE_ENV == undefined) {
+  process.env.NODE_ENV = 'development';
+}
+
+var files = [path.join(process.cwd(), '.env')]
+var file = path.join(process.cwd(), 'config', '.env.'+process.env.NODE_ENV);
+
+if (fs.existsSync(path.join(file))) {
+  files.push(file);
+}
+
+dotenv.config({ path: files, quiet: ['test', 'production'].indexOf(process.env.NODE_ENV) > -1 });
+
+
