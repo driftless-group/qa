9e91324e1fcc9eb352ae9dd80eea00ee32b07a1c
Scott Ballantyne <scottballantyne@protonmail.com>
2025-09-14 07:25:05 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const assert = require('assert');
+const crypto = require('crypto');
+
+
+const supertest = require('supertest');
+const express = require('express');
+
+const csrf = require('csurf');
+//const csrfProtection = csrf({cookie: true});
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+function appInstance(options={}) {
+  const instance = express();
+
+  if (options.render == undefined) {
+    options.render = true
+  }
+
+  if (options.parse == undefined) {
+    options.parse = true
+  }
+
+  if (options.render) {
+    instance.response.render = require('views/engine')({
+      views: [
+	path.join(__dirname, 'apps','hasLayout', 'views'),
+	path.join(__dirname, '..')
+      ],
+      stream: true
+    });
+  }
+
+  if (options.parse) {
+    instance.use(bodyParser.urlencoded())
+    instance.use(bodyParser.json());
+    instance.use(cookieParser())
+  }
+
+  if (options.csrf) {
+    instance.use(csrf({cookie: true}));
+    instance.use(function(req, res, next) {
+      res.locals.csrfToken = req.csrfToken();
+      next();
+    })
+  }
+
+  return instance;
+}
+
+module.exports.appInstance = appInstance;
+module.exports.supertest = supertest;
