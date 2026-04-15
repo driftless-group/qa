a3887bb0393749d67a5b1a4be93b2249648c8d0e
Scott Ballantyne <scottballantyne@protonmail.com>
2026-04-15 12:36:16 -0500

    removing things that are in a different module now

--- db/index.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-require(path.join(__dirname, '..', 'env'));
-var initialize = require(path.join(__dirname, 'initialize'));
-
-initialize(process.env.MONGO_URL).
-  catch(console.eror);
